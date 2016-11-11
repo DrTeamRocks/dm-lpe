768f0db0c042178fb61b36f0d37d1dac76c64ca1
Pavel Rykov <p.rykov@drteam.rocks>
2016-11-11 23:49:09 +0300

    sql

---
+++ backup/dm-lpe.mysq.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `dm`.`users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `email` text NOT NULL,
+    `username` text NOT NULL,
+    `password` text NOT NULL,
+    `lastlogin_time` text NOT NULL,
+    `enabled` bool NOT NULL DEFAULT TRUE,
+    `deleted` bool NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `dm`.`tokens` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `id_user` INT NOT NULL,
+    `hash` text NOT NULL,
+    `add_time` text NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `dm`.`sections` (
+    id INT NOT NULL AUTO_INCREMENT,
+    add_time text NOT NULL,
+    content text,
+    variables text,
+    ordering INT NOT NULL,
+    draft bool NOT NULL DEFAULT TRUE,
+    enabled bool NOT NULL DEFAULT TRUE,
+    deleted bool NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
