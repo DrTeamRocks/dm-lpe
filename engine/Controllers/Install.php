1126b37f8e3967f787664bd3b00c7292b8ab2ca0
Pavel Rykov <p.rykov@drteam.rocks>
2017-01-21 00:44:06 +0300

    install of system

---
+++ engine/Controllers/Install.php
@@ -0,0 +1,57 @@
+<?php namespace Application\Controllers;
+
+use System\Core\Controller;
+
+use Application\Models\Install as Model_Install;
+
+/**
+ * Class Install
+ * @package Application\Controllers
+ */
+class Install extends Controller
+{
+    public $_install;
+
+    /**
+     * Install constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->_install = new Model_Install();
+    }
+
+    /**
+     * Index page action
+     */
+    public function action_index()
+    {
+        $tables = array(
+            'users',
+            'roles',
+            'sites',
+            'settings',
+            'sections'
+        );
+
+        $i = 0;
+        while ($i < count($tables)) {
+            $name = $tables[$i];
+            if ($this->_install->table_exist($name)) {
+                echo "inf: Table `$name` exist<br/>\n";
+            } else {
+                echo "err: Table `$name` is not exist<br/>\n";
+
+                $create_name = 'create_' . $name;
+                $this->_install->$create_name();
+
+                if ($this->_install->table_exist($name)) echo "inf: Table `$name` created<br/>\n";
+                else echo "err: Table `$name` is not created<br/>\n";
+            }
+            $i++;
+        }
+
+    }
+
+}
